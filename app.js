2d5db21f31ae7d850773dbf789cedcdcd70fe8fe
Jessica Duell <duellrdh@gmail.com>
2018-08-19 21:55:57 -0400

    added if/else statement and a function

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ let name = 'Jessica';
 const numUSAstates = 50;
 let sum = (4+5); 
 
+// If first letter of name starts with L an alert will tell the user back of the line otherwise it'll alert the user with next
+if (name.charCodeAt(0) === 'L'){
+    alert("Back of the line!");
+} else {
+    alert("Next!");
+};
+
+//Function that displays an alert "Hello word!"
+function sayHello(){
+    alert("Hello World");
+};
+sayHello();
